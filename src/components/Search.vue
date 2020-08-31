0ffa8e6dc28db88736b71ea7eed67fe5034f2054
Lester Lin <370356141@qq.com>
2020-09-01 00:17:55 +0800

    add websocket demo

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -126,6 +126,7 @@ export default {
       }
     }
     return {
+      websock: null,
       parkingLotInfoRule: {
         name: [{validator: checkName, trigger: 'blur'}],
         address: [{validator: checkAddress, trigger: 'blur'}],
@@ -153,6 +154,12 @@ export default {
   },
   mounted () {
     this.getParkingLots()
+  },
+  created () {
+    this.initWebSocket()
+  },
+  destroyed () {
+    this.websock.close() // 离开路由之后断开websocket连接
   },
   methods: {
     handleSizeChange (val) {
@@ -250,6 +257,25 @@ export default {
       }).catch(e => {
         console.log(e)
       })
+    },
+    initWebSocket () { // 初始化weosocket
+      const wsuri = 'ws://127.0.0.1:8888/websocket'; this.websock = new WebSocket(wsuri); this.websock.onmessage = this.websocketonmessage; this.websock.onopen = this.websocketonopen; this.websock.onerror = this.websocketonerror; this.websock.onclose = this.websocketclose
+    },
+    websocketonopen () { // 连接建立之后执行send方法发送数据
+      let actions = {'test': '12345'}; this.websocketsend(JSON.stringify(actions))
+    },
+    websocketonerror () { // 连接建立失败重连
+      this.initWebSocket()
+    },
+    websocketonmessage (e) { // 数据接收
+      const redata = JSON.parse(e.data)
+      console.log(redata)
+    },
+    websocketsend (Data) { // 数据发送
+      this.websock.send(Data)
+    },
+    websocketclose (e) { // 关闭
+      console.log('断开连接', e)
     }
   }
 }
